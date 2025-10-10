f38ab99070fe233560b6682e4da633c1c0b5726c
Pablo <pabloallmeida123@gmail.com>
2025-10-09 21:04:34 -0300

    Atualizando informações do README

---
+++ build/CMakeFiles/hospital.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/pablo/Estudos USP/2o Semestre/Alg. e Estr. de Dados/hospital/history.cpp" "CMakeFiles/hospital.dir/history.cpp.obj" "gcc" "CMakeFiles/hospital.dir/history.cpp.obj.d"
+  "C:/Users/pablo/Estudos USP/2o Semestre/Alg. e Estr. de Dados/hospital/interface.cpp" "CMakeFiles/hospital.dir/interface.cpp.obj" "gcc" "CMakeFiles/hospital.dir/interface.cpp.obj.d"
+  "C:/Users/pablo/Estudos USP/2o Semestre/Alg. e Estr. de Dados/hospital/io.cpp" "CMakeFiles/hospital.dir/io.cpp.obj" "gcc" "CMakeFiles/hospital.dir/io.cpp.obj.d"
+  "C:/Users/pablo/Estudos USP/2o Semestre/Alg. e Estr. de Dados/hospital/patient.cpp" "CMakeFiles/hospital.dir/patient.cpp.obj" "gcc" "CMakeFiles/hospital.dir/patient.cpp.obj.d"
+  "C:/Users/pablo/Estudos USP/2o Semestre/Alg. e Estr. de Dados/hospital/patient_list.cpp" "CMakeFiles/hospital.dir/patient_list.cpp.obj" "gcc" "CMakeFiles/hospital.dir/patient_list.cpp.obj.d"
+  "C:/Users/pablo/Estudos USP/2o Semestre/Alg. e Estr. de Dados/hospital/wait_list.cpp" "CMakeFiles/hospital.dir/wait_list.cpp.obj" "gcc" "CMakeFiles/hospital.dir/wait_list.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hospital.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hospital.dir/history.cpp.obj"
+  "CMakeFiles/hospital.dir/history.cpp.obj.d"
+  "CMakeFiles/hospital.dir/interface.cpp.obj"
+  "CMakeFiles/hospital.dir/interface.cpp.obj.d"
+  "CMakeFiles/hospital.dir/io.cpp.obj"
+  "CMakeFiles/hospital.dir/io.cpp.obj.d"
+  "CMakeFiles/hospital.dir/patient.cpp.obj"
+  "CMakeFiles/hospital.dir/patient.cpp.obj.d"
+  "CMakeFiles/hospital.dir/patient_list.cpp.obj"
+  "CMakeFiles/hospital.dir/patient_list.cpp.obj.d"
+  "CMakeFiles/hospital.dir/wait_list.cpp.obj"
+  "CMakeFiles/hospital.dir/wait_list.cpp.obj.d"
+  "hospital.exe"
+  "hospital.exe.manifest"
+  "hospital.pdb"
+  "libhospital.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hospital.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
